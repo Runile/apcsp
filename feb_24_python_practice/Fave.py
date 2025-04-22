25d8f5b350a1000c88ae41a12795c267eeffe36d
Runile <danieljiang3511@gmail.com>
2025-04-22 14:21:22 -0400

    Delete feb_24_python_practice directory

--- feb_24_python_practice/Fave.py
+++
@@ -1,3 +0,0 @@
-print("whats ur fav class")
-answer = str(input())
-print(answer + " is my favorite class, too")
--- feb_24_python_practice/Squared.py
+++
@@ -1,3 +0,0 @@
-print("Input Number")
-num = float(input())
-print(str(num) + " squared is: " + str(num * num))
